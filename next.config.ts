fc9b7d84a703e33b970307a7dc96a00a93dfc52b
Elonatech-projects <elonatechprojects@elonatech.com.ng>
2025-07-23 08:28:17 +0100

    fix  404 error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // enables static export
+
   images: {
     unoptimized: true, // ✅ disables Next.js image optimization (required for static export)
   },

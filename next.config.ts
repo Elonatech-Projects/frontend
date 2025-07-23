b1b79420e0ce568c950ae6b9ecf9d5cf08eba52d
Elonatech-projects <elonatechprojects@elonatech.com.ng>
2025-07-23 09:40:02 +0100

    registration  404 error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   images: {
-    unoptimized: true, // ✅ disables Next.js image optimization (required for static export)
+    unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
